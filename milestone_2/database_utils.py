48104d0adedf41224c94cd3713666e5b385b1dfb
mahedjaved <mahed95@gmail.com>
2023-10-28 13:34:30 +0100

    updates gitignore

--- milestone_2/database_utils.py
+++ milestone_2/database_utils.py
@@ -1,5 +1,13 @@
+import yaml
+
 class DatabaseConnector:
     """
     @desc : use to connect with and upload data to the database.
     """
-    pass
+    
+
+
+    def read_db_creds():
+        """
+        @desc: reqs-> install PyYAML: pip install PyYAML
+        """
