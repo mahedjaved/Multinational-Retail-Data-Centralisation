5501aeb57e0b935829723575dcb68f7d81274c8a
mahedjaved <mahed95@gmail.com>
2023-10-28 14:35:43 +0100

    adds step 3 for task 2 of milestone 2

--- milestone_2/database_utils.py
+++ milestone_2/database_utils.py
@@ -1,13 +1,35 @@
+"""
+@reqs:
+    [1] PyYAML : !pip install PyYAML
+    [2] psycopg : !pip install psycopg2
+"""
+from sqlalchemy import create_engine
+import psycopg2
 import yaml
 
 class DatabaseConnector:
     """
     @desc : use to connect with and upload data to the database.
     """
-    
-
-
-    def read_db_creds():
+    def read_db_creds(self):
         """
         @desc: reqs-> install PyYAML: pip install PyYAML
         """
+        yaml_file_path = '../db_creds.yaml'
+
+        # Read the YAML file and store its contents in a Python data structure (dictionary)
+        with open(yaml_file_path, 'r') as file:
+            return yaml.safe_load(file)
+        
+
+    def init_db_engine(self):
+        yaml_data = self.read_db_creds()
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = yaml_data['RDS_HOST']
+        USER = yaml_data['RDS_USER']
+        PASSWORD = yaml_data['RDS_PASSWORD']
+        PORT = yaml_data['RDS_PORT']
+        DATABASE = yaml_data['RDS_DATABASE']
+
+        return create_engine(f"{DATABASE_TYPE}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
