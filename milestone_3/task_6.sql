12f6d237deb86bf780ec516f50244bac498d80bf
mahedjaved <mahed95@gmail.com>
2023-11-01 20:40:35 +0000

    adds task_6.sql for milestone_3, changes data types for the dim_date_times table

---
+++ milestone_3/task_6.sql
@@ -0,0 +1,18 @@
+CREATE TABLE 
+    new_dim_date_times AS
+SELECT
+    timestamp
+    ,CAST(month AS VARCHAR(2)) AS month
+    ,CAST(year AS VARCHAR(4)) AS year
+    ,CAST(day AS VARCHAR(2)) AS day
+    ,CAST(time_period AS VARCHAR(15))
+    ,CAST(date_uuid AS UUID) AS date_uuid
+    ,CAST(datetime AS TIME) AS datetime
+FROM 
+    dim_date_times
+
+DROP TABLE  
+    dim_date_times
+
+ALTER TABLE new_dim_date_times
+    RENAME TO dim_date_times;
